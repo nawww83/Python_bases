6b57c14d71d2c1407e54024f5cd238d8411693b7
nawww83 <nawww83@gmail.com>
2019-01-27 11:42:11 +0700

    Доделал д/з 3

--- Lesson_3/hw03_hard.py
+++ Lesson_3/hw03_hard.py
@@ -4,6 +4,8 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+# !!!Смотри в easy
+
 
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
@@ -12,6 +14,54 @@
 # то их ЗП уменьшается пропорционально, а за заждый час переработки
 # они получают удвоенную ЗП, пропорциональную норме.
 # Кол-во часов, которые были отработаны, указаны в файле "data/hours_of"
+
+import os
+
+pathW = os.path.join('data', 'workers')
+pathH = os.path.join('data', 'hours_of')
+
+moneys = {}
+titles = {}
+norms = {}
+hours = {}
+
+with open(pathW, 'r') as f:
+    lh = []
+    for line in f:
+        line = line.strip() # защита от пробельных строк
+        if line:
+            if not lh: # Header
+                lh = line.split()
+            else:
+                lw = line.split()
+                key = lw[0]+lw[1]
+                moneys[key] = float(lw[2])
+                titles[key] = lw[3]
+                norms[key] = float(lw[4])
+
+with open(pathH, 'r') as f:
+    lh = []
+    for line in f:
+        line = line.strip() # защита от пробельных строк
+        if line:
+            if not lh: # Header
+                lh = line.split()
+            else:
+                lw = line.split()
+                key = lw[0]+lw[1]
+                hours[key] = float(lw[2])
+
+moneys_total = {}
+
+for key, value in moneys.items():
+    h = hours[key]
+    hn = norms[key]
+    less = float(h <= hn)
+    up = float(int(h - hn))
+    moneys_total[key] = round(less * h/hn * value + (1 - less) * (up/hn + 1) * value, 2)
+
+for key in moneys_total:
+    print(key, titles[key], ' получит ', moneys_total[key])
 
 
 # Задание-3:
@@ -26,5 +76,24 @@
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+
+
+path = os.path.join('data', 'fruits.txt')
+lb = []
+dictF = {}
+with open(path, 'r') as f:
+    for line in f:
+        line = line.strip() # защита от пробельных строк
+        if line: 
+            b = line[0] # Первая буква
+            if not (b in lb):
+                lb.append(b)
+                pp = os.path.join('data', 'fruits_' + b + '.txt')
+                dictF[b] = open(pp, 'w')
+            dictF[b].write(line + '\n')
+
+for f in dictF:
+    dictF[f].close()
 
 
