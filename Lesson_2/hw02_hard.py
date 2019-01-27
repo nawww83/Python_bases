899e0cb86d829b463c8231e7e6c68bdcec3c02b5
nawww83 <nawww83@gmail.com>
2019-01-27 13:45:04 +0700

    Доделал hard

--- Lesson_2/hw02_hard.py
+++ Lesson_2/hw02_hard.py
@@ -2,3 +2,20 @@
 # Определить координату y точки с заданной координатой x.
 # вычислите и выведите y
 # например, equation = 'y = -12x + 11111140.2121'
+
+x = 4
+
+eq = 'y = - 12x + 48.0'
+
+eq = eq.replace(' ','')
+
+pos_equal = eq.index('=')
+pos_x = eq.index('x')
+
+k = float(eq[pos_equal+1:pos_x])
+bt = eq[pos_x+1:]
+
+b = float(bt)
+y = k * x + b
+
+print(k,b,y)
