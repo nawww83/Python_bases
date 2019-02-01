8cb7e8e7709e45835dc47671f526a793b2e90cc0
nawww83 <nawww83@gmail.com>
2019-02-01 13:56:45 +0700

    Сделал easy и normal

--- Lesson_4/hw04_easy.py
+++ Lesson_4/hw04_easy.py
@@ -6,12 +6,22 @@
 # квадратами элементов исходного списка
 # [0, 1, 2, 3] --> [0, 1, 4, 9]
 
+listA = [0, 1, 2, 3]
 
+listB = [el**2 for el in listA]
+
+print(listB)
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
+listA = ['banana', 'apple', 'qiwi', 'lemon']
+listB = ['orange', 'pineapple', 'apple', 'durian', 'qiwi']
+
+listC = [el for el in listA if el in listB]
+
+print(listC)
 
 
 # Задание-3:
@@ -21,7 +31,16 @@
 # + Элемент неотрицательный
 # + Элемент не кратен 3
 
+import random
 
+listA = [random.randint(-100,100) for _ in range(15)]
 
+print(listA)
 
+listB = [el for el in listA if not (el % 2)]
+listC = [el for el in listA if not (el < 0)]
+listD = [el for el in listA if (el % 3)]
 
+print(listB)
+print(listC)
+print(listD)
--- Lesson_4/hw04_normal.py
+++ Lesson_4/hw04_normal.py
@@ -4,7 +4,31 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
+import random
 
+strA = ''.join([chr(random.randint(65,122)) for _ in range(20)])
+
+print(strA)
+
+# a)
+pattern = '[a-z]+'
+listA = re.findall(pattern, strA)
+
+print(listA)
+
+# b)
+listB = list(strA)
+listC = []
+strB = ''
+for l in listB:
+    if l.islower():
+        strB = strB + l
+    elif strB:
+        listC.append(strB)
+        strB = ''
+
+print(listC)
 
 # Задание-2:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
@@ -13,3 +37,33 @@
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
 
+import os
+
+path = os.path.join('.','mn.txt')
+with open(path,'w') as f:
+    [f.write(str(random.randint(0,9))) for _ in range(2500)]
+
+mn = []
+with open(path) as f:
+    mn = f.readline()
+
+ln = []
+for i in range(0,9):
+    pattern_n = '[' + str(i) + ']+'
+    mn_n = re.findall(pattern_n, mn)
+    max_l = 0
+    for el in mn_n:
+        max_l = len(el) if (len(el) > max_l) else max_l
+    lnf = list(filter(lambda x: len(x) == max_l, mn_n))
+    if lnf:
+        ln.append(lnf[0])
+
+lnt = []
+max_l = 0
+for el in ln:
+    max_l = len(el) if (len(el) > max_l) else max_l
+lnf = list(filter(lambda x: len(x) == max_l, ln))
+if lnf:
+    lnt.append(lnf)
+
+print(lnt)
