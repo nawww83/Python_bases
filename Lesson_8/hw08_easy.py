19e18d978cdf958e4f192b5972a299e2a2db3cf2
nawww83 <nawww83@gmail.com>
2019-02-16 18:35:11 +0700

    Сделал easy и normal

--- Lesson_8/hw08_easy.py
+++ Lesson_8/hw08_easy.py
@@ -5,8 +5,16 @@
 элемента словаря, как к атрибуту класса.
 '''
 
-
-
+class Builder:
+    def __init__(self, l1, l2):
+        self.d = {}
+        for k,v in  zip(l1, l2):
+            self.d[k] = v
+            setattr(self, k, v)
+    
+b = Builder(['name','surname'],['Ivan','Ivanov'])
+print(b.name)
+print(b.surname)
 
 '''
 Задание 2.
@@ -17,4 +25,18 @@
 списка.
 '''
 
+class Wrapper:
+    def __init__(self, obj):
+        self.wrapped = obj
 
+    def __getattr__(self, attr):
+        return getattr(self.wrapped, attr)
+
+    def get(self):
+        return self.wrapped
+
+wl = Wrapper([1,2,3])
+print(wl.get())
+
+wl.clear()
+print(wl.get())
--- Lesson_8/hw08_normal.py
+++ Lesson_8/hw08_normal.py
@@ -7,3 +7,38 @@
 второго класса).
 '''
 
+class A:
+    def __init__(self, v1, v2):
+        self.B = B(v1, v2)
+
+    def res(self):
+        return self.B.res()
+
+    def div(self):
+        return self.B.div()
+
+    def idiv(self):
+        return self.B.idiv()
+
+
+class B:
+    def __init__(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+
+    def res(self):
+        return self.v1 % self.v2
+
+    def div(self):
+        return self.v1 / self.v2
+
+    def idiv(self):
+        return self.v1 // self.v2
+
+a1 = A(23,4)
+
+a2 = A(25,5)
+
+print(a1.res(), a1.div(), a1.idiv())
+print(a2.res(), a2.div(), a2.idiv())
+
