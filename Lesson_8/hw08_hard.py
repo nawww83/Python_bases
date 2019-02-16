79aab52d55f0b6a2b3007ed1cc923cc8496a8588
nawww83 <nawww83@gmail.com>
2019-02-16 19:03:02 +0700

    Сделал hard

--- Lesson_8/hw08_hard.py
+++ Lesson_8/hw08_hard.py
@@ -5,4 +5,46 @@
 (здесь можно применить встроенное исключение).
 '''
 
+class NonPositiveInputException(Exception):
+    def __init__(self):
+        Exception.__init__(self)
+
+    def __str__(self):
+        return 'NonPositiveInputException: введено отрицательное число. Ожидается положительное или ноль.'
+
+class PositiveNum:
+    def __init__(self, val):
+        try:
+            float(val)
+            if val < 0:              
+                raise NonPositiveInputException
+            else:
+                self.val = val
+        except ValueError:
+            print('Введено не число!')
+        else:
+            print('Все хорошо')
+        finally:
+            pass
+
+try:
+    p = PositiveNum(5)
+except NonPositiveInputException as np:
+    print(np)
+finally:
+    pass
+
+try:
+    p = PositiveNum(-3)
+except NonPositiveInputException as np:
+    print(np)
+finally:
+    pass
+
+try:
+    p = PositiveNum('a')
+except NonPositiveInputException as np:
+    print(np)
+finally:
+    pass
 
